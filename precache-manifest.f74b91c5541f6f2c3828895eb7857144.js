c8db3131d2848b826437170747dcafbf33328c22
Brand Traversy <gonesh923@gmail.com>
2020-07-29 01:55:05 +0600

    Updates

---
+++ precache-manifest.f74b91c5541f6f2c3828895eb7857144.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e735f8120a5569b8f5290eca6b510b9",
+    "url": "/Gonu923/crud-react-todo-app/index.html"
+  },
+  {
+    "revision": "658169b78b3d2c31e7f7",
+    "url": "/Gonu923/crud-react-todo-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ee58294d2a9b9f5c71c8",
+    "url": "/Gonu923/crud-react-todo-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "658169b78b3d2c31e7f7",
+    "url": "/Gonu923/crud-react-todo-app/static/js/2.4203e29b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Gonu923/crud-react-todo-app/static/js/2.4203e29b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee58294d2a9b9f5c71c8",
+    "url": "/Gonu923/crud-react-todo-app/static/js/main.1157baac.chunk.js"
+  },
+  {
+    "revision": "6350ee8a80ac749c4734",
+    "url": "/Gonu923/crud-react-todo-app/static/js/runtime-main.0c9440f9.js"
+  }
+]);
